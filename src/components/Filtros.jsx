3d97c21104597bdc2211a1c05cf60fb5da264265
Elyas RAJJAF <93983897+elyasrajjaf@users.noreply.github.com>
2022-09-07 16:11:04 +0200

    Publicar

---
+++ src/components/Filtros.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const Filtros = ({filtro, setFiltro}) => {
+  return (
+    <div className="filtros contenedor sombra">
+        <form>
+            <div className="campo">
+                <label>Filtrar Gastos</label>
+                <select
+                    value={filtro}
+                    onChange={e => setFiltro(e.target.value)}
+                >
+                    <option value="">-- Todas las categorias --</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="gastos">Gastos Varios</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="salud">Salud</option>
+                    <option value="sucscripciones">Sucscripciones</option>
+                </select>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Filtros
